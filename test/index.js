a44ac77711fc70dce627777909027b7bc70bfaf5
Aleen <aleen42@vip.qq.com>
2022-01-12 17:05:17 +0800

    perf(FormData): patches for `FormData`

--- test/index.js
+++ test/index.js
@@ -1,4 +1,7 @@
 require('../lib/polyfill');
 
+// implement `regeneratorRuntime` for asynchronous test cases
+require('@babel/polyfill');
+
 const testsContext = require.context('../lib', true, /-spec$/);
 testsContext.keys().forEach(testsContext);
