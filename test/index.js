527d43c15349c253c605f46ea9d2255363114ff4
Aleen <aleen42@vip.qq.com>
2025-01-26 18:06:05 +0800

    fix: Karma failed under IE8 due to page reloading

--- test/index.js
+++ test/index.js
@@ -1,3 +1,9 @@
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+
+const IE8_DOM_DEFINE = require('core-js/internals/ie8-dom-define');
+// karma kill IE8 with `client.clearContext` will trigger reload,
+// result in an error "Some of your tests did a full page reload"
+IE8_DOM_DEFINE && (window.onbeforeunload = _.noop);
+
 const testsContext = require.context('../lib', true, /-spec$/);
 testsContext.keys().forEach(testsContext);
